453164ada206146a7d6e918b09a4bd8230261781
dfsalam <dfsalam@gmail.com>
2023-03-17 00:56:50 -0500

    Add snapshot test

---
+++ src/testing/snapshot.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Detail from '../components/Detail';
+import Search from '../components/Search';
+import Town from '../components/Town';
+import TownContainer from '../components/TownContainer';
+import store from '../redux/store';
+
+const render = (component) => rtlRender(
+  <Provider store={store}>
+    {component}
+  </Provider>,
+);
+
+describe('Components render correctly', () => {
+  test('Detail', () => {
+    const tree = render(
+      <Detail />,
+    );
+    expect(tree).toMatchSnapshot();
+  });  
+  test('Search', () => {
+    const tree = render(<Search />);
+    expect(tree).toMatchSnapshot();
+  });
+  test('TownContainer', () => {
+    const tree = render(<TownContainer />);
+    expect(tree).toMatchSnapshot();
+  });
+});
